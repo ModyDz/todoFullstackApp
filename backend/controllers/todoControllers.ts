0ff67fe2e614c6341a61bdbfa6395528d4f9c323
ModyDz <79874169+ModyDz@users.noreply.github.com>
2022-05-28 08:19:39 +0100

    type added

--- backend/controllers/todoControllers.ts
+++ backend/controllers/todoControllers.ts
@@ -2,15 +2,17 @@ import asyncHandler from "express-async-handler";
 import { Request, Response } from "express";
 import User from "../models/userModel";
 import Todo from "../models/todoModel";
-interface IUser {
+type IUser = {
   username: string;
   email: string;
   password: string;
-  _id: string;
-}
+  id: string;
+} | null;
 export const newTodo = asyncHandler(async (req: Request, res: Response) => {
   const { username } = req.body;
-  const user: any = await User.findOne({ username });
-  const newTodo = await Todo.create({ text: "Hello world", user: user.id });
+  const user: IUser = await User.findOne({ username });
+  if (user) {
+    const newTodo = await Todo.create({ text: "Hello world", user: user.id });
+  }
   res.send(newTodo);
 });
